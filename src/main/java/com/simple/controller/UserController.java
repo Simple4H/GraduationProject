2cf8bca8b5751d3d0ca30f009d10916f56a1c208
S I M P L E <crescentcxm@gmail.com>
2017-12-15 15:34:03 +0800

    修改

--- src/main/java/com/simple/controller/UserController.java
+++ src/main/java/com/simple/controller/UserController.java
@@ -6,9 +6,7 @@ import com.simple.pojo.User;
 import com.simple.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -24,22 +22,12 @@ public class UserController {
 
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(@RequestBody String username, @RequestBody String password, HttpSession session) {
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
     }
-//    @RequestMapping(value = "login.do", method = RequestMethod.POST)
-//    @ResponseBody
-//    public ServerResponse<User> login(@RequestBody Map map, HttpSession session) {
-//        String username = (String) map.get("username");
-//        String password = (String) map.get("password");
-//        ServerResponse<User> response = iUserService.login(username, password);
-//        if (response.isSuccess()) {
-//            session.setAttribute(Const.CURRENT_USER, response.getData());
-//        }
-//        return response;
-//    }
+
 }
