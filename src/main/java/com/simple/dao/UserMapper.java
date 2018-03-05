aa30d6eaef2854afbdf3cd52b84cd16d65db0217
S I M P L E <crescentcxm@gmail.com>
2018-03-05 10:54:28 +0800

    待修改

--- src/main/java/com/simple/dao/UserMapper.java
+++ src/main/java/com/simple/dao/UserMapper.java
@@ -1,7 +1,6 @@
 package com.simple.dao;
 
 import com.simple.pojo.User;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -15,22 +14,4 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
-
-    int checkUsername(String username);
-
-    User userLogin(@Param("username") String username, @Param("password") String password);
-
-    int checkPassword(@Param("password") String password, @Param("userId") int userId);
-
-    int checkEmailByUserId(@Param("email") String email, @Param("userId") int userId);
-
-    int checkQuestionAndAnswer(@Param("username") String username, @Param("question") String password, @Param("answer") String answer);
-
-    int updatePasswordByUsername(@Param("username") String username, @Param("newPassword") String newPassword);
-
-    int checkEmail(String email);
-
-    int checkAdmin(String username);
-
-    String getQuestion(String username);
 }
